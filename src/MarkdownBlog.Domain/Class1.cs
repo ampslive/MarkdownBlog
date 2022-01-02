e1d1cc6c5bb5cd9c22b35bbf7c2c08b469004938
ampslive <amitphilips@live.com>
2022-01-03 00:01:09 +0530

    Base solution

--- src/MarkdownBlog.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MarkdownBlog.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ src/MarkdownBlog.Domain/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace MarkdownBlog.Domain.Models
+{
+    public class Post
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Permalink { get; set; }
+        public string ContentHTML { get; set; }
+        public string ContentMarkDown { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public bool IsActive { get; set; }
+        public List<Author> Authors { get; set; }
+    }
+}
