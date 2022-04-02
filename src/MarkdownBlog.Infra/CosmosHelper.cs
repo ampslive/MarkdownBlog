5cae50036a27361179fde95479709050af7a33a5
Amit Philips <amitphilips@live.com>
2022-04-02 22:47:19 +0530

    Note

--- src/MarkdownBlog.Infra/CosmosHelper.cs
+++ src/MarkdownBlog.Infra/CosmosHelper.cs
@@ -8,5 +8,6 @@ namespace MarkdownBlog.Infra
 {
     internal class CosmosHelper
     {
+        //https://docs.microsoft.com/en-us/learn/modules/use-azure-cosmos-db-sql-api-sdk/4-connect-to-online-account
     }
 }
