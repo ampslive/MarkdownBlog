52904009591b8bb90213be4c8b8abbd684fce81f
Amit Philips <amitphilips@live.com>
2022-04-02 22:42:14 +0530

    Added BlogStore

--- src/MarkdownBlog.Domain/Store/BlogStore.cs
+++ src/MarkdownBlog.Domain/Store/BlogStore.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MarkdownBlog.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,14 @@ namespace MarkdownBlog.Domain.Store
 {
     public class BlogStore
     {
+        public BlogStore()
+        {
+
+        }
+
+        public async Task<Blog> Add(Blog blog)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ src/MarkdownBlog.Infra/CosmosConfig.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Azure.Cosmos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkdownBlog.Infra
+{
+    public class CosmosConfig
+    {
+        // Replace <documentEndpoint> with the information created earlier
+        public static readonly string EndpointUri = "<documentEndpoint>";
+
+        // Set variable to the Primary Key from earlier.
+        private static readonly string PrimaryKey = "<your primary key>";
+
+        // The Cosmos client instance
+        public CosmosClient cosmosClient;
+
+        // The database we will create
+        private Database database;
+
+        // The container we will create.
+        private Container container;
+
+        // The names of the database and container we will create
+        private string databaseId = "az204Database";
+        private string containerId = "az204Container";
+    }
+}
