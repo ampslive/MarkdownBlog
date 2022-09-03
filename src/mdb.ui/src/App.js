eb67a0c770cffbc2929903ac2a662e2bf016b9d6
Amit Philips <amitphilips@live.com>
2022-09-04 02:28:18 +0530

    Added Bootstrap CSS

--- src/mdb.ui/src/App.js
+++ src/mdb.ui/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button>Click me</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
