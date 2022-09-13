68c7ef2e8ae73cfa582365651be7d5a2a99b1684
Amit Philips <amitphilips@live.com>
2022-09-13 23:12:34 +0530

    Basic folder structure created

--- src/mdb.ui/src/App.js
+++ src/mdb.ui/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import TopNav from './TopNav'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NewsSources from './NewsSources'
-import Blog from './Blog'
+import Blog from './pages/blog'
 
 function App() {
   return (
--- src/mdb.ui/src/NewsSources.js
+++ src/mdb.ui/src/NewsSources.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ListGroup from 'react-bootstrap/ListGroup';
-import { GetApi } from './ApiHelper'
+import { GetApi } from './common/ApiHelper'
 
 class NewsSources extends React.Component {
 
