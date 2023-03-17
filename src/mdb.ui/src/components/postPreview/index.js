f4848583bdd16dbbb7f44c10057cd417e36e082f
Amit Philips <amitphilips@live.com>
2023-03-18 00:27:40 +0530

    Include required columns in post preview

--- src/mdb.ui/src/components/postPreview/index.js
+++ src/mdb.ui/src/components/postPreview/index.js
@@ -1,18 +1,26 @@
 import React, { Fragment } from 'react'
+import './style.css'
 
 function PostPreview(props) {
     return (
         <Fragment>
             <div class="card">
-                <img src={props.image} class="card-img-top" alt="Post Banner" />
+                <img src={props.post.bannerUri} class="card-img-top" alt="Post Banner" />
                 <div class="card-body">
-                    <pre class="text-right text-uppercase blockquote-footer">{props.author.name}</pre>
-                    <a class="nav-link card-title" href="/news"><h5>{props.title}</h5></a>
-                    <p class="card-text">{props.description}</p>
+                    <pre class="text-right text-uppercase">{props.post.blogName}</pre>
+                    <a class="nav-link card-title" href="/news"><h5>{props.post.title}</h5></a>
+                    <p class="card-text">{props.post.description}</p>
+                    
+                    <div class="d-flex justify-content-between text-muted">
+                        <div class="d-flex justify-content-between">
+                            <img src={props.post.author.imageUri} alt="author" class="user-image"/>
+                            <p class="px-2 mt-1">{props.post.author.name}</p>
+                        </div>
+                        <div><p class="px-2 mt-1"><small>{props.post.dateCreated}</small></p></div>
+                    </div>
+
                 </div>
             </div>
         </Fragment>
     );
 } 
-
-export default PostPreview;
