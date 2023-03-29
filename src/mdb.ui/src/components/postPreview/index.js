3872e32f57d6a8dc98eaf0499419ad41cda3ac10
Amit Philips <amitphilips@live.com>
2023-03-29 18:03:38 +0530

    #10 Navigation to post pages

--- src/mdb.ui/src/components/postPreview/index.js
+++ src/mdb.ui/src/components/postPreview/index.js
@@ -1,19 +1,31 @@
 import React, { Fragment } from 'react'
 import './style.css'
+import { Link } from 'react-router-dom';
 
 function PostPreview(props) {
+
+    const postUri = `/post/${props.post.id}`;
+
     return (
         <Fragment>
             <div class="card">
-                <img src={props.post.bannerUri} class="card-img-top" alt="Post Banner" />
+                <Link to={postUri}>
+                    <img src={props.post.bannerUri} class="card-img-top" alt="Post Banner" />
+                </Link>
+
                 <div class="card-body">
                     <pre class="text-right text-uppercase">{props.post.blogName}</pre>
-                    <a class="nav-link card-title" href="/news"><h5>{props.post.title}</h5></a>
+                    <a class="nav-link card-title" href={postUri} ><h5>{props.post.title}</h5></a>
+
+                    <Link to={postUri}>
+                        <img src={props.post.bannerUri} class="card-img-top" alt="Post Banner" />
+                    </Link>
+
                     <p class="card-text">{props.post.description}</p>
-                    
+
                     <div class="d-flex justify-content-between text-muted">
                         <div class="d-flex justify-content-between">
-                            <img src={props.post.author.imageUri} alt="author" class="user-image"/>
+                            <img src={props.post.author.imageUri} alt="author" class="user-image" />
                             <p class="px-2 mt-1">{props.post.author.name}</p>
                         </div>
                         <div><p class="px-2 mt-1"><small>{props.post.dateCreated}</small></p></div>
@@ -23,6 +35,6 @@ function PostPreview(props) {
             </div>
         </Fragment>
     );
-} 
+}
 
 export default PostPreview;
