868a0ef57bfdac3465f1b8a70cfb8fceb9e868ef
Amit Philips <amitphilips@live.com>
2023-04-04 16:39:27 +0530

    #11 Elements of Post added

--- src/mdb.ui/src/pages/post/index.js
+++ src/mdb.ui/src/pages/post/index.js
@@ -1,33 +1,72 @@
 import React, { Fragment } from 'react'
-//import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom';
 import BlogData from '../../blogsData.json'
 
 function Post(props) {
 
-    /*
-    const [post, setPost] = useState([]);
+    const defaultPost = {
+        bannerUri: "https://picsum.photos/1000/300",
+        blogName: "Blog Series",
+        body: "This is my third blog post.",
+        dateCreated: "09/17/2022 18:46:07",
+        description: "Subtext",
+        id: "0",
+        title: "Sample Data",
+        author: {
+            id: 1,
+            name: "Amit Philips",
+            imageUri: "https://i.pravatar.cc/40?img=1"
+        }
+    };
+
+    const [blogPost, setPost] = useState(defaultPost);
+    let { id } = useParams();
+
 
     useEffect(() => {
-        var data = props.post;
-        setPost(data);
-    }, [])
-    */
 
-    let { id } = useParams();
+        var data = [];
+        BlogData.blogs.map(x => x.posts.map(y => data.push(ConvertToPosts(x, y))));
+        let filteredPost = data.filter(function (x) { return x.id == id; });
+        setPost(filteredPost[0]);
+    }, [])
+
+    function ConvertToPosts(blog, post) {
+        return {
+            id: post.id,
+            blogName: blog.title,
+            title: post.title,
+            bannerUri: post.bannerUri,
+            description: post.description,
+            body: post.body,
+            dateCreated: post.dateCreated,
+            author: {
+                id: post.author.id,
+                name: post.author.name,
+                imageUri: post.author.imageUri
+            }
+        }
+    }
+
+
+
 
     return (
         <Fragment>
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-11 my-4 borderRed">
-                        <div>Post Id: {id} Title</div>
-                        <div>author details</div>
-                        <div>post meta</div>
-                        <div>post body</div>
+            {blogPost && (
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-11 my-4 borderRed">
+                            <div>{blogPost.title}</div>
+                            <div>{blogPost.author.name}</div>
+                            <div>{blogPost.dateCreated}</div>
+                            <div>{blogPost.bannerUri}</div>
+                            <div>{blogPost.body}</div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            )}
         </Fragment>
 
         /*
