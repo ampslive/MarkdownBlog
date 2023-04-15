e0270c9f3a63556da8996d373aa72c74b6b324ac
Amit Philips <amitphilips@live.com>
2023-04-15 17:02:46 +0530

    #19 Added more descriptions in date formatter

--- src/mdb.ui/src/common/Utils.js
+++ src/mdb.ui/src/common/Utils.js
@@ -1,5 +1,26 @@
 export const formatDate = (date) => {
     const options = { year: 'numeric', month: 'short', day: 'numeric' };
-    var dt = new Date( Date.parse(date));
-    return new Intl.DateTimeFormat('en-US', options).format(dt);
+    let dt = new Date(Date.parse(date));
+    let currentDate = new Date();
+
+    let timeDiff = Math.abs(currentDate.getTime() - dt.getTime());
+    let diffDays = Math.floor(timeDiff / (1000 * 3600 * 24));
+    //alert(diffDays);
+    switch (true) {
+        case (diffDays == 0):
+            return 'Today';
+
+        case (diffDays == 1):
+            return 'Yesterday';
+
+        case (diffDays < 7):
+            return diffDays + 'd ago';
+
+        case (diffDays >= 7 && diffDays <= 13):
+            return '1wk ago';
+
+        default:
+            return new Intl.DateTimeFormat('en-US', options).format(dt);
+
+    }
 }
