133faf736226f57fad78fabb61939c40fdf9c8be
Amit Philips <amitphilips@live.com>
2023-04-27 17:40:54 +0530

    Added MinimalAPI Proj with an empty BlogGroup #25

---
+++ src/MarkdownBlog.API/BlogGroup.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+
+public static class BlogGroup
+{
+    public static IEndpointRouteBuilder MapBlogApi(this IEndpointRouteBuilder group)
+    {
+        group.MapGet("/posts", GetPosts);
+        return group;
+    }
+
+    public static string GetPosts()
+    {
+        return "Hello Blog";
+        //return TypedResults.Created($"{todo.Id}", todo);
+    }
+}
--- src/MarkdownBlog.API/Program.cs
+++ src/MarkdownBlog.API/Program.cs
@@ -1,55 +1,9 @@
-using MarkdownBlog.Domain.Contracts;
-using MarkdownBlog.Domain.Repositories;
-using MarkdownBlog.Domain.Store;
-using MarkdownBlog.Infra;
-using Microsoft.Azure.Cosmos;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddSingleton<CosmosConfig>(new CosmosConfig(
-        builder.Configuration.GetSection("CosmosConnection:Endpoint").Value,
-        builder.Configuration.GetSection("CosmosConnection:DbName").Value
-    ));
-
-//Cosmos Client
-builder.Services.AddSingleton<CosmosClient>(new CosmosClient(
-    builder.Configuration.GetSection("CosmosConnection:Endpoint").Value, 
-    builder.Configuration.GetSection("CosmosConnection:Key").Value,
-    new CosmosClientOptions()
-    {
-        SerializerOptions = new CosmosSerializationOptions()
-        {
-            PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase,
-            IgnoreNullValues = true
-        }
-    }
-    ));
-
-builder.Services.AddTransient<IDatabaseHelper, CosmosHelper>();
-
-//repos
-builder.Services.AddTransient<BlogRepo>();
-
-//store
-builder.Services.AddTransient<BlogStore>();
-
-var serviceProvider = builder.Services.BuildServiceProvider();
-
-var containerList = new Dictionary<string, string> { 
-    { "Blogs", "id" }, 
-};
-
-var dbName = builder.Configuration.GetSection("CosmosConnection:DbName").Value;
-
-builder.Services.CreateDB(dbName, serviceProvider.GetService<CosmosClient>())
-                .CreateContainer(dbName, containerList, serviceProvider.GetService<CosmosClient>());
 
 var app = builder.Build();
 
@@ -62,8 +16,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.MapGet("/api", () => "Hello World")
+//.WithName("Say Hello")
+//.WithOpenApi();
 
-app.MapControllers();
+app.MapBlogApi();
 
 app.Run();
