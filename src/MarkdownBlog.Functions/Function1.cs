4eaebceb2159dd0cd77f47c39f0e4f84c283571f
Amit Philips <amitphilips@live.com>
2023-10-21 06:10:28 +0530

    #41 Setup Middleware Pipeline

--- src/MarkdownBlog.Functions/Function1.cs
+++ src/MarkdownBlog.Functions/Function1.cs
@@ -1,35 +1,30 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
-namespace MarkdownBlog.Functions
+namespace MarkdownBlog.Functions;
+
+public class Function1
 {
-    public static class Function1
+    private readonly ILogger _logger;
+
+    public Function1(ILoggerFactory loggerFactory)
     {
-        [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+        _logger = loggerFactory.CreateLogger<Function1>();
+    }
 
-            string name = req.Query["name"];
+    [Function("Function1")]
+    public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+        response.WriteString("Welcome to Azure Functions!");
 
-            return new OkObjectResult(responseMessage);
-        }
+        return response;
     }
 }
+
---
+++ src/MarkdownBlog.Functions/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Azure;
+using MarkdownBlog.Domain.Store;
+using MarkdownBlog.Infra;
+using Microsoft.Extensions.Configuration;
+using MarkdownBlog.Domain.Contracts;
+using MarkdownBlog.Domain.Models;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
+using Azure.Storage.Blobs;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices((context, services) =>
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(context.HostingEnvironment.ContentRootPath)
+            .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        services.AddAzureClients(clientBuilder =>
+        {
+            clientBuilder.AddBlobServiceClient(configuration["StorageConnectionString"]);
+        });
+
+        services.AddScoped<IBlobContext<BlogMaster>, Context>(serviceProvider =>
+        {
+            var blobStoreHelper = serviceProvider.GetRequiredService<BlobStoreHelper>();
+
+            var data = JsonSerializer.Deserialize<BlogMaster>(blobStoreHelper.GetBlobAsync().Result);
+
+            var ctx = new Context(blobStoreHelper, data);
+            return ctx;
+        });
+
+        services.AddTransient<AuthorStore>();
+        services.AddTransient<BlogSeriesStore>();
+        services.AddTransient<PostStore>();
+
+        services.AddSingleton<BlobStoreHelper>(serviceProvider =>
+        {
+            var blobServiceClient = serviceProvider.GetRequiredService<BlobServiceClient>();
+            string containername = configuration["BlobContainerName"];
+            string filename = configuration["BlobFileName"];
+            return new BlobStoreHelper(blobServiceClient, containername, filename);
+        });
+    })
+.Build();
+
+BlobInitializer.CreateContainerAndBlob(host.Services);
+
+host.Run();
