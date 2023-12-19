c1be0b15b2a85c38bd5a85d46958b8ec2c25d95e
Amit Philips <amitphilips@live.com>
2023-12-19 21:07:05 +0530

    Code Cleanup

--- src/MarkdownBlog.Functions/PostFunction.cs
+++ src/MarkdownBlog.Functions/PostFunction.cs
@@ -8,95 +8,94 @@ using Microsoft.Extensions.Options;
 using MarkdownBlog.API.Models;
 using MarkdownBlog.Domain.Models;
 
-namespace MarkdownBlog.Functions
+namespace MarkdownBlog.Functions;
+
+public class PostFunction
 {
-    public class PostFunction
+    private readonly PostStore _store;
+    private readonly JsonSerializerOptions _serializerOptions;
+    private readonly ILogger _logger;
+
+    public PostFunction(PostStore store, IOptions<JsonSerializerOptions> serializerOptions, ILoggerFactory loggerFactory)
     {
-        private readonly PostStore _store;
-        private readonly JsonSerializerOptions _serializerOptions;
-        private readonly ILogger _logger;
+        _store = store;
+        _serializerOptions = serializerOptions.Value;
+        _logger = loggerFactory.CreateLogger<PostFunction>();
+    }
 
-        public PostFunction(PostStore store, IOptions<JsonSerializerOptions> serializerOptions, ILoggerFactory loggerFactory)
+    [Function("PostFunction")]
+    public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", "delete", Route = "posts/{id?}")] HttpRequestData req)
+    {
+        HttpResponseData response = req.Method switch
         {
-            _store = store;
-            _serializerOptions = serializerOptions.Value;
-            _logger = loggerFactory.CreateLogger<PostFunction>();
+            "GET" => await GetPosts(req),
+            "POST" => await CreatePost(req),
+            "DELETE" => await DeletePost(req),
+            _ => req.CreateResponse(HttpStatusCode.MethodNotAllowed)
+        };
+
+        return response;
+    }
+
+    private async Task<HttpResponseData> DeletePost(HttpRequestData req)
+    {
+        string? id = req.Url.Segments.Length > 3 ? req.Url.Segments[3] : default;
+
+        var result = await _store.RemovePost(id);
+
+        if (result == null)
+        {
+            var responseNotFound = req.CreateResponse(HttpStatusCode.NotFound);
+            await responseNotFound.WriteAsJsonAsync(result);
+            return responseNotFound;
         }
 
-        [Function("PostFunction")]
-        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post", "delete", Route = "posts/{id?}")] HttpRequestData req)
-        {
-            HttpResponseData response = req.Method switch
-            {
-                "GET" => await GetPosts(req),
-                "POST" => await CreatePost(req),
-                "DELETE" => await DeletePost(req),
-                _ => req.CreateResponse(HttpStatusCode.MethodNotAllowed)
-            };
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        await response.WriteAsJsonAsync(result);
 
-            return response;
+        return response;
+    }
+
+    private async Task<HttpResponseData> CreatePost(HttpRequestData req)
+    {
+        var content = await new StreamReader(req.Body).ReadToEndAsync();
+        var model = JsonSerializer.Deserialize<PostRequest>(content, _serializerOptions);
+
+        var post = new Post()
+        {
+            Title = model.Title,
+            BannerUri = model.BannerUri,
+            Description = model.Description,
+            Body = model.Body,
+            DateCreated = DateTime.UtcNow,
+            AuthorIds = model.AuthorIds,
+            Meta = model.Meta,
+            Series = model.Series
+        };
+
+        var result = await _store.AddPost(post);
+
+        var response = req.CreateResponse(HttpStatusCode.Created);
+        await response.WriteAsJsonAsync(result);
+
+        return response;
+    }
+
+    private async Task<HttpResponseData> GetPosts(HttpRequestData req)
+    {
+        string? id = req.Url.Segments.Length > 3 ? req.Url.Segments[3] : default;
+        var result = await _store.GetPosts(id);
+
+        if (result == null)
+        {
+            var responseNotFound = req.CreateResponse(HttpStatusCode.NotFound);
+            await responseNotFound.WriteAsJsonAsync(result);
+            return responseNotFound;
         }
 
-        private async Task<HttpResponseData> DeletePost(HttpRequestData req)
-        {
-            string? id = req.Url.Segments.Length > 3 ? req.Url.Segments[3] : default;
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        await response.WriteAsJsonAsync(result);
 
-            var result = await _store.RemovePost(id);
-
-            if (result == null)
-            {
-                var responseNotFound = req.CreateResponse(HttpStatusCode.NotFound);
-                await responseNotFound.WriteAsJsonAsync(result);
-                return responseNotFound;
-            }
-
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            await response.WriteAsJsonAsync(result);
-
-            return response;
-        }
-
-        private async Task<HttpResponseData> CreatePost(HttpRequestData req)
-        {
-            var content = await new StreamReader(req.Body).ReadToEndAsync();
-            var model = JsonSerializer.Deserialize<PostRequest>(content, _serializerOptions);
-
-            var post = new Post()
-            {
-                Title = model.Title,
-                BannerUri = model.BannerUri,
-                Description = model.Description,
-                Body = model.Body,
-                DateCreated = DateTime.UtcNow,
-                AuthorIds = model.AuthorIds,
-                Meta = model.Meta,
-                Series = model.Series
-            };
-
-            var result = await _store.AddPost(post);
-
-            var response = req.CreateResponse(HttpStatusCode.Created);
-            await response.WriteAsJsonAsync(result);
-
-            return response;
-        }
-
-        private async Task<HttpResponseData> GetPosts(HttpRequestData req)
-        {
-            string? id = req.Url.Segments.Length > 3 ? req.Url.Segments[3] : default;
-            var result = await _store.GetPosts(id);
-
-            if (result == null)
-            {
-                var responseNotFound = req.CreateResponse(HttpStatusCode.NotFound);
-                await responseNotFound.WriteAsJsonAsync(result);
-                return responseNotFound;
-            }
-
-            var response = req.CreateResponse(HttpStatusCode.OK);
-            await response.WriteAsJsonAsync(result);
-
-            return response;
-        }
+        return response;
     }
 }
