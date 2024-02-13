336317db36420df29f84dab84cbdb782a0d54a0b
Amit Philips <amitphilips@live.com>
2024-02-13 23:05:07 +0530

    #64 Added specs

--- src/mdb.ui/src/App.js
+++ src/mdb.ui/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import TopNav from './components/TopNav'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import NewsSources from './NewsSources'
 import Blog from './pages/blog'
 import Sample from './pages/sample/sample';
@@ -9,21 +9,26 @@ import Filter from './pages/filter';
 
 function App() {
   return (
-      <div className="App">
-        <Router>
-          <TopNav title="AMIT PHILIPS" />
-          <div className="content">
-            <Routes>
-              <Route path="/" element={<Blog />} />
-              <Route path="/news" element={<NewsSources />} />
-              <Route path="/blog" element={<Blog />} />
-              <Route path="/blog/:filter/:searchTerm" element={<Filter />} />
-              <Route path="/sample" element={<Sample />} />
-              <Route path="/post/:id/:title" element={<Post />} />
-            </Routes>
+    <div className="App">
+      <Router>
+        <TopNav title="AMIT PHILIPS" />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Blog />} />
+            <Route path="/news" element={<NewsSources />} />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:filter/:searchTerm" element={<Filter />} />
+            <Route path="/sample" element={<Sample />} />
+            <Route path="/post/:id/:title" element={<Post />} />
+          </Routes>
+        </div>
+        <footer class="footer mt-auto py-3 bg-light text-center fs-6">
+          <div class="container">
+            <span class="fw-lighter">This site was developed on React 18, Azure Function 4, .NET 8, hosted on Azure. To know more visit the repo for <a target='_blank' href="https://github.com/ampslive/MarkdownBlog">MarkDown Blog</a></span>
           </div>
-        </Router>
-      </div>
+        </footer>
+      </Router>
+    </div>
   );
 }
 
