f9b8fccff6d2361a5bb21f0a4317cc5b37a885b1
Amit Philips <amitphilips@live.com>
2024-02-14 19:04:39 +0530

    #64 added rel attribute

--- src/mdb.ui/src/App.js
+++ src/mdb.ui/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import TopNav from './components/TopNav'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import NewsSources from './NewsSources'
 import Blog from './pages/blog'
 import Sample from './pages/sample/sample';
@@ -24,7 +24,7 @@ function App() {
         </div>
         <footer class="footer mt-auto py-3 bg-light text-center fs-6">
           <div class="container">
-            <span class="fw-lighter">This site was developed on React 18, Azure Function 4, .NET 8, hosted on Azure. To know more visit the repo for <a target='_blank' href="https://github.com/ampslive/MarkdownBlog">MarkDown Blog</a></span>
+            <span class="fw-lighter">This site was developed on React 18, Azure Function 4, .NET 8, hosted on Azure. To know more visit the repo for <a target='_blank' rel="noopener noreferrer" href="https://github.com/ampslive/MarkdownBlog">MarkDown Blog</a></span>
           </div>
         </footer>
       </Router>
